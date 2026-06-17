311130f8f5ade72390ecd452ff2d8cdee565f470
kmiracles <miraclekambai2@gmail.com>
2026-06-17 13:57:48 +0100

    fix: clerk login signin and app context recursion bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import {AppContextProvider} from "@/context/AppContext";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -19,10 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+      <AppContextProvider>
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
       {children}
     </body>
     </html>
+    </AppContextProvider>
+    </ClerkProvider>
   );
 }
